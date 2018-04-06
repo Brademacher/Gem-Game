6e50f5bc51f668de5e9383dd1c829632eb8dc1fe
Ben <benjamin.rademacher@gmail.com>
2018-04-06 14:36:03 -0500

    added a few buttons and console.logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,48 +1,65 @@
-//Create arrays for crystal value and random number value.
+//Create global arrays for wins and losses.
+var winCount = 0;
+var lossCount = 0;
+var currentTotal = 0;
+
+//Function to start game, and place hidden value inside target
+function inititializeGame() {
+    //Create array with min value of 19 and max of 130
+    var targetValue = Math.floor(Math.random() * Math.floor(110)+20);
+    console.log("this random value is " + targetValue);
+    var targetContainer = $("#target");
+    targetContainer.innerHTML = "";
+    // need to finish this function
+}
+
+//Array for buttons.
 var crystals = [
     "crystal1",
     "crystal2",
     "crystal3",
     "crystal4"
 ];
+var btnContainer = document.getElementById('crystal');
 
-//Create array with min value of 19 and max of 130
-var targetValue = [];
-for (var i = 19; i <= 130; i++) {
-    console.log("targetValue works");
-    targetValue.push(i);
-}
-
-var btnContainer = $("crystal");
-
-//Create buttons for each of the crystals.
+//Function to create buttons for each of the crystals.
 function createButtons() {
     for (var i = 0; i < crystals.length; i++) {
         //variables used in function
         var currentCrystal = crystals[i];
         var crystalButton = document.createElement('button');
+        //Give buttons an image
+        for (var j = 0; j < crystals.length; j++) {
+            currentCrystal[j].innerHTML = '<img src="./assets/css/Images/Crystal.png" alt="Cystal-Image">';
+        }
         
-        //apply styling here
-        $("#crystals").addClass('crystal-button');
-        $("#crystals").addClass('btn');
-        $("#crystals").addClass('btn-outline-info');
+        //Function to assign random values to each crystal (1-12).
+        var crystalValue = Math.floor(Math.random() * Math.floor(12)+1);
+        console.log("this random value is " + crystalValue);
 
+        //apply styling here
+        $("#crystal").addClass('crystal-button');
+        $("#crystal").addClass('btn');
+        $("#crystal").addClass('btn-outline-info');
+        $("#crystal").addClass('hide-value');
+
+        $("#crystal").innerHTML = currentCrystal;
+        
         //apply event listener
-        crystalButton.value = currentCrystal;
-        crystalButton.on("click", addValue);
-        console.log("you can press " + (this));
+        crystalButton.value = crystalValue;
+        crystalButton.addEventListener("click", addValue);
+        btnContainer.appendChild(crystalButton);
     }
 }
 
 createButtons();
 
-//Function to generate target value (19-130) and insert it into the target box.
-
-
-    //assign classes to crystal so they can be given stylings.
-
-    //Function to assigns random values to each crystal (1-12).
-
+//Function to add value of crystal pressed to total
+function addValue () {
+    var userTotal = parseInt(currentTotal) + parseInt(cystalValue);
+    console.log(addValue);
+    return userTotal;
+}
 //onClick of each crystal do if/else function to determine if the current value is <, ==, or > target value.
     //if (current < target) {
        // return;
