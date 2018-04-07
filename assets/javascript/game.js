21566c6acee79f5c5c6b8cde491a5be8c6f93f9c
Ben <benjamin.rademacher@gmail.com>
2018-04-06 21:27:42 -0500

    all logic done

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,78 +2,92 @@
 var winCount = 0;
 var lossCount = 0;
 var currentTotal = 0;
-
+var targetValue = 0;
+var isGameInProgress = true;
+    //Array for buttons.
+    var crystals = ["crystal1", "crystal2", "crystal3", "crystal4"];
 
 //Function to start game, and place hidden value inside target
-function inititializeGame() {
+function initializeGame() {
     //Create array with min value of 19 and max of 130
-    var targetValue = Math.floor(Math.random() * Math.floor(110) + 20);
+    targetValue = Math.floor(Math.random() * Math.floor(110) + 20);
     console.log("target random value is " + targetValue);
-    var targetContainer = $("#target");
-    $("#target").innerHTML = targetValue;
-    // need to finish this function
+    $("#target").html(targetValue);
+    currentTotal = 0;
+    $("#currentTotal").html(currentTotal);
+    $("#count").html('Wins:' + winCount + '/' + 'Losses:' + lossCount);
+    $('#wins').hide();
+    $('#losses').hide();
+    checkGameStatus();
+    isGameInProgress = true;
+    assignCrystalValues();
 }
 
-var btnContainer = document.getElementById('crystal');
+
 
 //Function to create buttons for each of the crystals.
 function createButtons() {
+    var btnContainer = document.getElementById('crystal');
 
-    //Array for buttons.
-    var crystals = ["crystal1", "crystal2", "crystal3", "crystal4"];
     for (var i = 0; i < crystals.length; i++) {
         //variables used in function
         var currentCrystal = crystals[i];
-        var crystalButton = document.createElement('button');
-
-        //Function to assign random values to each crystal (1-12).
-        var crystalValue = Math.floor(Math.random() * Math.floor(12) + 1);
-        console.log("this random value is " + crystalValue);
+        var crystalButton = document.createElement('img');
+        crystalButton.src = './assets/css/Images/Crystal.png'
 
         //apply styling here
-        $("#crystal").addClass('crystal-button');
-        $("#crystal").addClass('btn');
-        $("#crystal").addClass('btn-outline-info');
-        $("#crystal").addClass('hide-value');
-
-        crystalButton.innerHTML = currentCrystal;
+        $(crystalButton).addClass('crystal-button');
+        $(crystalButton).addClass('btn');
+        $(crystalButton).addClass('hide-value');
+        $(crystalButton).attr('id', currentCrystal);
 
         //apply event listener
-        crystalButton.value = crystalValue;
         crystalButton.addEventListener("click", addValue);
         btnContainer.appendChild(crystalButton);
     }
+}
 
+function assignCrystalValues() {
+    for (var i = 0; i < crystals.length; i++) {
+      //Function to assign random values to each crystal (1-12).
+      var crystalValue = Math.floor(Math.random() * Math.floor(12) + 1);
+      console.log("this random value is " + crystalValue);
+      $("#"+crystals[i]).attr('data-value', crystalValue);
+    }
 }
 
 //Function to add value of crystal pressed to total
-function addValue() {
-    var userTotal = parseInt(currentTotal) + parseInt(btnContainer.value);
-    console.log(userTotal);
-    return userTotal;
+function addValue(ev) {
+    console.log(this);
+    currentTotal = parseInt(currentTotal) + parseInt($(this).attr('data-value'));
+    console.log(currentTotal);
+    $("#currentTotal").html(currentTotal);
+    checkGameStatus();
+
 }
 
 function checkGameStatus() {
-    var gameWon = true;
-    //If user is still below targetValue
-    if (currentTotal < targetValue) {
-        gameWon = false; 
-        break;
+
+    if (!isGameInProgress) {
+        return;
     }
     //If user adds correctly
-    else if (currentTotal == targetValue) {
-        var winner = document.getElementById('winner');
-            winner.style.display= "block";
-            winCount++;
+    if (currentTotal == targetValue) {
+        $('#wins').show();
+        winCount++;
+        isGameInProgress = false;
+
     }
+
     //If user user goes over targetValue
-    else if (currentTotal > targetValue) {
-        var loser = document.getElementById('loser');
-            loser.style.display= "block";
-            lossCount++;
+    if (currentTotal > targetValue) {
+        $('#losses').show();
+        lossCount++;
+        isGameInProgress = false;
+
     }
+    $("#count").html('Wins:' + winCount + '/' + 'Losses:' + lossCount);
 }
 
-inititializeGame();
 createButtons();
-checkGameStatus();
+initializeGame();
